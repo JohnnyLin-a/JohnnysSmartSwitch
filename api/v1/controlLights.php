d71565c20fdabe352bceb2ff4a471e14052bb614
Johnny Lin <johnny.lin.prime@gmail.com>
2019-12-10 16:29:25 -0500

    Fix typo for close light in controlLights.php

--- api/v1/controlLights.php
+++ api/v1/controlLights.php
@@ -21,7 +21,7 @@ function close() {
     global $env;
     
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $env['OPEN_LIGHT_IFTTT']);
+    curl_setopt($ch, CURLOPT_URL, $env['CLOSE_LIGHT_IFTTT']);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $output = curl_exec($ch);
     curl_close($ch);
